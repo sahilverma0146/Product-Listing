4835cb3a88c0002077e3f4f6da500112b54ed887
Sahil <sahilverma22146@gmail.com>
2024-08-25 19:32:07 +0530

    Create Data.js

---
+++ product-listing-app/src/Data/Data.js
@@ -0,0 +1,16 @@
+const Data=[
+    {
+        id:Date.now(),
+        productname : 'Redmi note 10',
+        price:2000,
+       
+    },
+    {
+        id:Date.now(),
+        productname : 'Iphone 14 ',
+        price:2000,
+    },
+    
+]
+
+export default Data;
